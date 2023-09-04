5f2d1aa515bd97c88aa6dbf5325e030f98884301
Nell <xianbin.zhu@spacemit.com>
2023-09-04 13:45:34 +0800

    hotplug: we'd better flush the local & l2 cache when one cpu going to die
    Change-Id: I64e4158a8669af577abf1ab5018a7bdb424941aa

--- arch/riscv/kernel/cpu-hotplug.c
+++ arch/riscv/kernel/cpu-hotplug.c
@@ -75,8 +75,14 @@ void __noreturn arch_cpu_idle_dead(void)
 {
 	idle_task_exit();
 
+#if defined(CONFIG_SOC_SPACEMIT_K1PRO) || defined(CONFIG_SOC_SPACEMIT_K1X)
+	sbi_flush_local_dcache_all();
+#endif
 	cpuhp_ap_report_dead();
 
+#if defined(CONFIG_SOC_SPACEMIT_K1PRO) || defined(CONFIG_SOC_SPACEMIT_K1X)
+	sbi_flush_local_dcache_all();
+#endif
 	cpu_ops[smp_processor_id()]->cpu_stop();
 	/* It should never reach here */
 	BUG();
