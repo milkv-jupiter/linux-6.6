ff952ff4ef19401d5c63c55de22ac760646bc22c
王楚 <chu.wang@spacemit.com>
2023-09-27 19:25:54 +0800

    add tcm_cfg_save/restore
    Change-Id: Idb486570aeecbf530d3cee1d5a363d4b2b04bb7c

--- arch/riscv/include/asm/csr.h
+++ arch/riscv/include/asm/csr.h
@@ -393,6 +393,11 @@
 #define CSR_VTYPE		0xc21
 #define CSR_VLENB		0xc22
 
+#ifdef CONFIG_SOC_SPACEMIT_K1X
+/* TCM enable register */
+#define CSR_TCMCFG		0x5db
+#endif
+
 #ifdef CONFIG_RISCV_M_MODE
 # define CSR_STATUS	CSR_MSTATUS
 # define CSR_IE		CSR_MIE
@@ -512,6 +517,11 @@
 			      : : "rK" (__v)			\
 			      : "memory");			\
 })
+
+#ifdef CONFIG_SOC_SPACEMIT_K1X
+#define tcm_config_save()		csr_read(CSR_TCMCFG)
+#define tcm_config_restore(flags)	csr_set(CSR_TCMCFG, flags)
+#endif
 
 #endif /* __ASSEMBLY__ */
 
