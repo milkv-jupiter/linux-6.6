175b2613964215390740c54ce628dce1622fceca
wanlong <long.wan@spacemit.com>
2024-11-15 11:30:34 +0800

    usb: typec: husb239: enable wakeup source in i2c_device_probe() instead of husb239
    Change-Id: I040e01a7ce7e3c9eaa0052b8961185ab5f361ed0

--- drivers/usb/typec/husb239.c
+++ drivers/usb/typec/husb239.c
@@ -1159,11 +1159,6 @@ static int husb239_probe(struct i2c_client *client)
 		goto err_unreg_switch;
 	}
 
-	if (of_property_read_bool(husb239->dev->of_node, "wakeup-source")) {
-		device_init_wakeup(husb239->dev, true);
-		dev_pm_set_wake_irq(husb239->dev, husb239->gpio_irq);
-	}
-
 	return 0;
 
 err_unreg_switch:
@@ -1181,11 +1176,6 @@ static void husb239_remove(struct i2c_client *client)
 
 	if (husb239->workqueue)
 		destroy_workqueue(husb239->workqueue);
-
-	if (device_may_wakeup(husb239->dev)) {
-		dev_pm_clear_wake_irq(husb239->dev);
-		device_init_wakeup(husb239->dev, false);
-	}
 
 	typec_switch_unregister(info->sw);
 	typec_unregister_port(info->port);
